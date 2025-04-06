621743ce5608d848a1d24517178fb41ec700605d
Alex22sv <ae22mp@gmail.com>
2025-04-06 09:02:29 -0600

    Remove auth middleware from plan route in public route

--- src/plan/plan.route.js
+++ src/plan/plan.route.js
@@ -8,8 +8,8 @@ import { app_config } from '../config/app.config.js';
 
 const plan_router = express.Router();
 
-plan_router.get('/', auth_middleware, plan_controller.get_all_plans);
-plan_router.get('/:id', auth_middleware, plan_validator.plan_validation_rules_get_id, validate, plan_controller.get_plan_by_id);
+plan_router.get('/', plan_controller.get_all_plans);
+plan_router.get('/:id', plan_validator.plan_validation_rules_get_id, validate, plan_controller.get_plan_by_id);
 plan_router.post('/', auth_middleware, validate_permission(app_config.PERMISSIONS.CREATE_PLAN), plan_validator.plan_validation_rules_post, validate, plan_controller.create_plan);
 plan_router.put('/:id', auth_middleware, validate_permission(app_config.PERMISSIONS.UPDATE_PLAN), plan_validator.plan_validation_rules_update, validate, plan_controller.update_plan);
 plan_router.delete('/:id', auth_middleware, validate_permission(app_config.PERMISSIONS.DELETE_PLAN), plan_validator.plan_validation_rules_get_id, validate, plan_controller.delete_plan);
