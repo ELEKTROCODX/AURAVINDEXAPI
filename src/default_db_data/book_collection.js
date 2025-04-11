055cb3000324f14a212ba7c4a702261aaf6c2422
Alex22sv <ae22mp@gmail.com>
2025-04-11 11:34:47 -0600

    Update default data

--- src/default_db_data/book_collection.js
+++ src/default_db_data/book_collection.js
@@ -5,13 +5,8 @@ export const book_collections = [
     {"name": "Reserved"}, 
     {"name": "Children"}, 
     {"name": "Braille"}, 
-    {"name": "Tsuzuki"}, 
     {"name": "Reference"},
-    {"name": "F. Mauricio Gaborit"}, 
-    {"name": "Fine Arts and Architecture"}, 
     {"name": "Music"},
     {"name": "Thesis"}, 
-    {"name": "S.A.P."},
     {"name": "Newspaper archive"},
-    {"name": "Venezuela"}
 ];
--- src/default_db_data/user.js
+++ src/default_db_data/user.js
@@ -6,7 +6,7 @@ export const users = [
     "name": "Aura",
     "last_name": "Vindex",
     "email": app_config.app_admin_email,
-    "biography": "Administrator of Aura Vindex",
+    "biography": "Administrator of AURA VINDEX.",
     "gender": "Other",
     "birthdate": "2024-01-01",
     "role": "Administrator",
