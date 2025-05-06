d6fffe8169269bb2cf2c9073db97463a0fb5626d
Alex22sv <ae22mp@gmail.com>
2025-05-05 19:59:33 -0600

    Update again default data

--- src/default_db_data/book_collection.js
+++ src/default_db_data/book_collection.js
@@ -6,6 +6,7 @@ export const book_collections = [
     {"name": "Children"}, 
     {"name": "Braille"}, 
     {"name": "Reference"},
+    {"name": "Music"},
     {"name": "Thesis"}, 
     {"name": "Newspaper archive"},
 ];
