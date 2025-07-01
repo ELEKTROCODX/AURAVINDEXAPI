24dc2143273613e38dcd049a819267e220e70be3
Alex22sv <ae22mp@gmail.com>
2025-07-01 17:24:00 -0600

    Increase limit for image sizes

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ const __dirname = path.dirname(__filename);
 const app = express();
 // Middleware
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 // Serve static files from the 'public' directory
 app.use('/images/', express.static(path.join(__dirname, 'public/images')));
